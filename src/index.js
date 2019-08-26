4f87015f2d9ef4060a7dcc8a393057ff75af8d8e
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-08-25 17:59:51 -0700

    Add logrocket script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import LogRocket from 'logrocket'
+
 import App from './App'
+
+LogRocket.init('mlmogy/recipes')
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
